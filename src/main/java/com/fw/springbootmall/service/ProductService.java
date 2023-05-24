dca014b800dd462ce73a50572eb0c642f2baf28b
xuexibianc <1325963774@qq.com>
2023-05-24 11:16:05 +0800

    2023-5-24 commit mall项目 商品功能-实作查询商品列表(基础实作)

--- src/main/java/com/fw/springbootmall/service/ProductService.java
+++ src/main/java/com/fw/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.fw.springbootmall.service;
 import com.fw.springbootmall.dto.ProductRequest;
 import com.fw.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
