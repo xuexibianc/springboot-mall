cd13d6eb52473ca822491f521e863638f80557ba
xuexibianc <1325963774@qq.com>
2023-05-29 15:17:43 +0800

    2023-5-29 commit mall项目 账号功能-使用MD5加密密码

--- src/main/java/com/fw/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/fw/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -34,7 +35,12 @@ public class UserServiceImpl implements UserService {
            log.warn("该 email {} 已经被注册",userRegisterRequest.getEmail());
            throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
        }
-       //创建账号
+
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+
+        userRegisterRequest.setPassword(hashedPassword);
+
+        //创建账号
         return userDao.createUser(userRegisterRequest); //很贴近要做的事情
 
     }
@@ -44,12 +50,17 @@ public class UserServiceImpl implements UserService {
 
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        //检查user是否存在
         if (user == null){
             log.warn("该 email {} 尚未注册",userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+        //使用 MD5 生成密码的杂凑值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        //比较密码
+        if (user.getPassword().equals(hashedPassword)){
             return user;
         }else {
             log.warn("email {} 的密码不正确",userLoginRequest.getEmail());
